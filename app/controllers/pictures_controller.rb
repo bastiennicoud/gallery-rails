434590d728bb35ed0616826fae3e1fc30e368f57
Bastien <bastien.nicoud@gmail.com>
2018-03-19 13:58:09 +0100

    S3 upload ok

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -57,7 +57,7 @@ class PicturesController < ApplicationController
   def destroy
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.html { redirect_to gallery_pictures_url, notice: 'Picture was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,15 +1,7 @@
 class Picture < ApplicationRecord
-  has_attached_file :path, storage: :s3, s3_credentials: Proc.new{|a| a.instance.s3_credentials }
+  has_attached_file :path, path: "bastien/:id/:filename"
+  validates_attachment_content_type :path, content_type: /\Aimage\/.*\z/
   validates :title, presence: true
-  validates :path, presence: true
 
   belongs_to :gallery
-
-  def s3_credentials
-    {
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  end
 end
